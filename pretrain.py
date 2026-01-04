23293d4b0d568d58170b55c175d626224244eb55
Kashif <haque.kashiful7@gmail.com>
2026-01-04 21:48:39 +0530

    Adjust training parameters for batch size and grad accum

--- pretrain.py
+++ pretrain.py
@@ -27,9 +27,9 @@ from utils import (
 @dataclass
 class TrainArgs:
     data_dir: str = "data_bin"
-    batch_size: int = 64
+    batch_size: int = 128
     block_size: int = 2048
-    grad_accum: int = 8
+    grad_accum: int = 4
     lr: float = 3e-4
     max_iters: int = 5725
     warmup_iters: int = 900
